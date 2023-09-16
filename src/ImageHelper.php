a75ed4513c4651c03bd61587b2cda0a67a3673ab
nguyenanhung <info@nguyenanhung.com>
2023-09-16 15:27:08 +0700

    Update Project at Sat Sep 16 15:27:08 +07 2023

--- src/ImageHelper.php
+++ src/ImageHelper.php
@@ -325,8 +325,8 @@ class ImageHelper
     public static function formatImageUrl($input = '', $server = '', $base = 'live')
     {
         $images_url = trim($input);
-        $images_url = bear_framework_crawler_reformat_url_link_need_change_http_to_https($input);
-        $images_url = bear_framework_crawler_reformat_url_link_need_change_domain($input);
+        $images_url = bear_framework_crawler_reformat_url_link_need_change_http_to_https($images_url);
+        $images_url = bear_framework_crawler_reformat_url_link_need_change_domain($images_url);
         try {
             if (function_exists('base_url') && function_exists('config_item') && !empty($images_url)) {
                 $no_thumb = array(
