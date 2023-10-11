bbebd15c132ee5f86d88fdf641696c97d062ca7a
nguyenanhung <info@nguyenanhung.com>
2023-10-12 00:32:33 +0700

    Update Project at Thu Oct 12 00:32:33 +07 2023

--- src/ImageHelper.php
+++ src/ImageHelper.php
@@ -251,11 +251,10 @@ class ImageHelper
 
         $protocol = array($schema . '://', '//',);
         $imageUrl = str_replace($protocol, '', $imageUrl);
-        $imageUrl = str_replace($urlPath . '?', $urlPath, $imageUrl);
         if (!empty($urlQuery)) {
             $imageUrl = str_replace('?' . $urlQuery, '', $imageUrl);
         }
-
+        $imageUrl = str_replace($urlPath . '?', $urlPath, $imageUrl);
         $server = trim($server);
         if (in_array($server, self::wordpressProxyProxyServerList())) {
             $proxyUrl = 'https://' . trim($server) . '.wp.com/';
